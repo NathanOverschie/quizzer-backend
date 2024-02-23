e890ab9875a0a4158a76570749a6749c46baa04a
nathan <nathan.overschie@gmail.com>
2024-02-23 16:19:27 +0100

    change endpoint to accept json

--- src/main/java/com/example/quizzer/QuestionsController.java
+++ src/main/java/com/example/quizzer/QuestionsController.java
@@ -4,10 +4,13 @@ import com.example.quizzer.dataproviders.OpentdbHandler;
 import com.example.quizzer.dataproviders.OpentdbQuestionProvider;
 import com.example.quizzer.utils.CorrectAnswerNotFoundException;
 import com.example.quizzer.utils.NotEnoughQuestionsException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +21,7 @@ public class QuestionsController {
                             new OpentdbHandler()),
                     1000);
 
-    @GetMapping("/questions")
+    @GetMapping(value = "/questions", produces = "application/json")
     public ResponseEntity<?> Questions(){
         try {
             return ResponseEntity.ok(manager.getQuestionsWithPossibleAnswers(5));
@@ -27,13 +30,22 @@ public class QuestionsController {
         }
     }
 
-    @PostMapping("/checkanswers")
+    @PostMapping(value = "/checkanswers", consumes = "application/json", produces = "application/json")
     public ResponseEntity<?> CheckAnswers(
-            @RequestParam int ID,
-            @RequestParam String answer
-    ) {
-        try {
+                @RequestBody String body
+            ) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try{
+            JsonNode jsonNode = mapper.readTree(body);
+
+            int ID = jsonNode.get("ID").asInt();
+            String answer = jsonNode.get("answer").asText();
+
             return ResponseEntity.ok(manager.checkAnswer(ID, answer));
+
+        } catch (JsonProcessingException | NullPointerException e) {
+            return ResponseEntity.badRequest().body("malformed json");
         } catch (CorrectAnswerNotFoundException e) {
             return ResponseEntity.badRequest().body("Correct answer was not found");
         }
