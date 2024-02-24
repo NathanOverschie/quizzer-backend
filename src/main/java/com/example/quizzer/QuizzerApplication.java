566820c74f7d2e56417563d13f7d6ecbd2215e54
nathan <nathan.overschie@gmail.com>
2024-02-24 12:53:41 +0100

    add jar

--- src/main/java/com/example/quizzer/QuizzerApplication.java
+++ src/main/java/com/example/quizzer/QuizzerApplication.java
@@ -1,12 +1,15 @@
 package com.example.quizzer;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class QuizzerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuizzerApplication.class, args);
+		new SpringApplicationBuilder(QuizzerApplication.class)
+				.web(WebApplicationType.SERVLET)
+				.run(args);
 	}
 }
